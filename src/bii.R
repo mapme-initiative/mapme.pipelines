5846d53e5bb4040aff81ffccc1047ddf8cac0630
Petutschnig, Andreas <Andreas.Petutschnig@adesso.de>
2024-06-05 12:01:03 +0200

    add biodiv intactness index

---
+++ src/bii.R
@@ -0,0 +1,42 @@
+source("src/000_setup.R")
+
+fetch_bii <- function(x, fname_bii = "bii.tif", progress = TRUE) {
+  if(!file.exists(fname_bii)) {
+    stop(sprintf("Biodiversity intactness index data needs to be downloaded manually. Link: %s",
+         "https://data.nhm.ac.uk/dataset/global-map-of-the-biodiversity-intactness-index-from-newbold-et-al-2016-science"
+         ))
+  }
+  with_progress({
+    get_resources(x, get_biodiversity_intactness_index(fname_bii))
+  }, enable = progress)
+}
+
+stats_bii <- function(
+    x,
+    progress = TRUE) {
+
+  with_progress({
+    inds <- calc_indicators(
+      x,
+      calc_biodiversity_intactness_index()
+    )
+  }, enable = progress)
+
+  inds
+}
+
+timings <- run_indicator(
+  country_codes = country_codes,
+  wdpa_src = wdpa_dsn,
+  layer = layer,
+  fetch_resources = fetch_bii,
+  calc_stats = stats_bii,
+  resource_cores = 10,
+  ncores = ncores,
+  progress = progress,
+  area_threshold = 50000,
+  out_path = out_path,
+  suffix = "bii-indicators"
+)
+
+saveRDS(timings, file.path(out_path, "bii-timings.rds"))
