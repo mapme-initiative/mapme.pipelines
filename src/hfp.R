bc0f4c6f3b44f10261fc27b32fa4e0e4bdbf464f
Darius A. Görgen <info@dariusgoergen.com>
2024-06-05 11:15:12 +0200

    Adds humanfootprint script

---
+++ src/hfp.R
@@ -0,0 +1,39 @@
+source("src/000_setup.R")
+
+fetch_hfp <- function(x, progress = TRUE) {
+  with_progress({
+    get_resources(x, get_humanfootprint(years = 2000:2020))
+  }, enable = progress)
+}
+
+stats_hfp <- function(
+    x,
+    progress = TRUE,
+    stats = c("min", "mean", "median", "sd", "max", "sum")) {
+
+  with_progress({
+    inds <- calc_indicators(
+      x,
+      calc_humanfootprint(engine = "exactextract",
+                          stats = stats)
+    )
+  }, enable = progress)
+
+  inds
+}
+
+timings <- run_indicator(
+  country_codes = country_codes,
+  wdpa_src = wdpa_dsn,
+  layer = layer,
+  fetch_resources = fetch_hfp,
+  calc_stats = stats_hfp,
+  resource_cores = 10,
+  ncores = ncores,
+  progress = progress,
+  area_threshold = 50000,
+  out_path = out_path,
+  suffix = "hfp-indicators"
+)
+
+saveRDS(timings, file.path(out_path, "hfp-timings.rds"))
